2dd20af3d933bc342a2cbee09e22293a66e6ca47
priyeshraj20 <priyeshraj20@gmail.com>
2025-06-18 22:20:31 +0530

    Setting up the HTML,CSS & JS files and starting up

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputDisplay = document.getElementById("input-display");
+const oneBtn = document.getElementById("1-button");
+const twoBtn = document.getElementById("2-button");
+const threeBtn = document.getElementById("3-button");
+const fourBtn = document.getElementById("4-button");
+const fiveBtn = document.getElementById("5-button");
+const sixBtn = document.getElementById("6-button");
+const sevenBtn = document.getElementById("7-button");
+const eightBtn = document.getElementById("8-button");
+const nineBtn = document.getElementById("9-button");
+const zeroBtn = document.getElementById("0-button");
+
+
+const buttonToDisplay = () => {
+    
+}

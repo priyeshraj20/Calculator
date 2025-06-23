ebaff0effbf11aebec0ce6c487b522166b9993e6
priyeshraj20 <priyeshraj20@gmail.com>
2025-06-23 19:24:43 +0530

    Creating button inputs and clearing of input display

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ const sevenBtn = document.getElementById("7-button");
 const eightBtn = document.getElementById("8-button");
 const nineBtn = document.getElementById("9-button");
 const zeroBtn = document.getElementById("0-button");
+const clearBtn = document.getElementById("clear-button");
+
+inputDisplay.innerText = "";
 
 
-const buttonToDisplay = () => {
-    
+const buttonToDisplay = (value) => {   
+inputDisplay.innerText += value;
 }
+
+const clearDisplay = () => {
+    inputDisplay.innerText = "";
+}
+
+
+oneBtn.addEventListener("click", ()=>buttonToDisplay(oneBtn.value));
+twoBtn.addEventListener("click", ()=>buttonToDisplay(twoBtn.value));
+threeBtn.addEventListener("click", ()=>buttonToDisplay(threeBtn.value));
+fourBtn.addEventListener("click", ()=>buttonToDisplay(fourBtn.value));
+fiveBtn.addEventListener("click", ()=>buttonToDisplay(fiveBtn.value));
+sixBtn.addEventListener("click", ()=>buttonToDisplay(sixBtn.value));
+sevenBtn.addEventListener("click", ()=>buttonToDisplay(sevenBtn.value));
+eightBtn.addEventListener("click", ()=>buttonToDisplay(eightBtn.value));
+nineBtn.addEventListener("click", ()=>buttonToDisplay(nineBtn.value));
+zeroBtn.addEventListener("click", ()=>buttonToDisplay(zeroBtn.value));
+
+
+clearBtn.addEventListener("click", ()=>clearDisplay());

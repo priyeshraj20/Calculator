c1c66e6527b013b289a2209ccf1fae7283ce5530
priyeshraj20 <priyeshraj20@gmail.com>
2025-06-23 23:38:57 +0530

    Added event listeners to all buttons

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const eightBtn = document.getElementById("8-button");
 const nineBtn = document.getElementById("9-button");
 const zeroBtn = document.getElementById("0-button");
 const clearBtn = document.getElementById("clear-button");
+const plusBtn = document.getElementById("plus-button");
+const minusBtn = document.getElementById("minus-button");
+const multiplyBtn = document.getElementById("multiply-button");
+const divisionBtn = document.getElementById("division-button");
+const equalBtn = document.getElementById("equal-button");
 
 inputDisplay.innerText = "";
 
@@ -23,6 +28,12 @@ const clearDisplay = () => {
 }
 
 
+//calculator functions
+
+const addition = () => {
+
+};
+
 oneBtn.addEventListener("click", ()=>buttonToDisplay(oneBtn.value));
 twoBtn.addEventListener("click", ()=>buttonToDisplay(twoBtn.value));
 threeBtn.addEventListener("click", ()=>buttonToDisplay(threeBtn.value));
@@ -33,6 +44,27 @@ sevenBtn.addEventListener("click", ()=>buttonToDisplay(sevenBtn.value));
 eightBtn.addEventListener("click", ()=>buttonToDisplay(eightBtn.value));
 nineBtn.addEventListener("click", ()=>buttonToDisplay(nineBtn.value));
 zeroBtn.addEventListener("click", ()=>buttonToDisplay(zeroBtn.value));
+clearBtn.addEventListener("click", ()=>clearDisplay());
+
+plusBtn.addEventListener("click",()=>{
+    buttonToDisplay(plusBtn.value);
+});
+
+minusBtn.addEventListener("click", ()=>{
+    buttonToDisplay(minusBtn.value);
+});
+
+multiplyBtn.addEventListener("click", ()=>{
+    buttonToDisplay(multiplyBtn.value);
+});
+
+divisionBtn.addEventListener("click", ()=>{
+    buttonToDisplay(divisionBtn.value);
+});
+
+equalBtn.addEventListener("click", ()=>{
+   return; 
+});
 
 
 clearBtn.addEventListener("click", ()=>clearDisplay());
